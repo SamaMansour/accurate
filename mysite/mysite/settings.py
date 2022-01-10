0902f761cd8ccdfec64ce8a1866668d8ca9811e6
Sama Mansour <samamansour306@gmail.com>
2022-01-10 22:22:40 +0200

    10 jan

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -142,11 +142,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = '1025'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 's.jamansour@gmail.com'
-EMAIL_HOST_PASSWORD= 'Hatethis@4050'
-EMAIL_USE_TLS=False
+EMAIL_HOST_PASSWORD = 'Lovethis@4050'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
